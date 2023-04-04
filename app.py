f415cdb7706459765a012b1a3dd29ddb83e77934
maxsiriwat <104423020+maxsiriwat@users.noreply.github.com>
2023-04-04 07:19:52 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,25 +3,42 @@ import pandas as pd
 import numpy as np
 import pickle as pickle
 
-store = pickle.load(open("clf","rb" ) )
+store = pickle.load(open("model","rb" ) )
 
 st.header("ทำนายความเสี่ยงของการตกเลือด")
 st.write("")
-Age = st.number_input('Age (อายุ)',step=1,key='None')
-Gestation = st.number_input('Gestation (จำนวนการตั้งครรภ์ รวมปัจจุบัน รวมแท้ง)',key='None')
-Parity = st.number_input('Parity (จำนวนลูกที่คลอดออกมาได้)',key='None')
-GA = st.number_input('GA (อายุครรภ์ต่อสัปดาห์)',key='None')
-height = st.number_input("height (ความสูง) ซม.",value=1)
-weight = st.number_input("weight (น้ำหนัก) กก.",value=1)
+Age = st.number_input('Age (อายุ)',min_value=1,max_value=100,step=1,value=20,key='None')
+Gestation = st.number_input('Gestation (จำนวนการตั้งครรภ์ รวมปัจจุบัน รวมแท้ง)',min_value=1,max_value=20,value=1,key='None')
+Parity = st.number_input('Parity (จำนวนลูกที่คลอดออกมาได้)',min_value=0,max_value=20,value=1,key='None')
+GA = st.number_input('GA (อายุครรภ์ต่อสัปดาห์)',min_value=1,max_value=100,value=30,key='None')
+Hct = st.number_input('Hct (ความเข้มข้นเลือด)',min_value=1,max_value=100,value=30,key='None')
+height = st.number_input("height (ความสูง) ซม.",min_value=1,max_value=250,value=150)
+weight = st.number_input("weight (น้ำหนัก) กก.",min_value=1,max_value=200,value=50)
 BMI = weight / (height/100)**2
 BMI = st.number_input('BMI (ค่า BMI)',value=BMI,key='None')
-HxPPH = st.number_input('Hx PPH (เคยตกเลือดมาก่อนในท้องที่แล้ว)',key='None')
-DM = st.number_input('DM (เป็นเบาหวานในท้องนี้)',key='None')
-PIH = st.number_input('PIH (มีประวัติความดันสูงขณะตั้งครรภ์)',key='None')
+HxPPH = st.radio('Hx PPH (เคยตกเลือดมาก่อนในท้องที่แล้ว)',('เคย','ไม่เคย'),key='None')
+if HxPPH == 'เคย':
+    HxPPH = 1
+elif HxPPH == 'ไม่เคย':
+    HxPPH = 0
+DM = st.radio('DM (เป็นเบาหวานในท้องนี้)',('เป็น','ไม่เป็น'),key='None')
+if DM == 'เป็น':
+    DM = 1
+elif DM == 'ไม่เป็น':
+    DM = 0
+PIH = st.radio(
+    "PIH (มีประวัติความดันสูงขณะตั้งครรภ์)",
+    ('มี','ไม่มี'))
+if PIH == 'มี':
+    PIH = 1
+elif PIH == 'ไม่มี':
+    PIH = 0
 submit = st.button('Predict')
 
+
+
 if submit:
-    y = store.predict([[Age,Gestation,Parity,GA,BMI,HxPPH,DM,PIH]])
+    y = store.predict([[Age,Gestation,Parity,GA,Hct,BMI,HxPPH,DM,PIH]])
     if Age == 0:
         st.info('กรุณากรอกข้อมูลให้ครบถ้วน (Age)')
     elif Gestation == 0:
